27f61d2a7ba246a4a686aa2a62ff9ac171ac3811
Richard <28573353+ivmidable@users.noreply.github.com>
2026-02-17 14:42:45 -0500

    Execute MVP localnet tests

--- five-templates/cpi-integration-tests/test-pda-burn.v
+++ five-templates/cpi-integration-tests/test-pda-burn.v
@@ -7,24 +7,11 @@ interface SPLToken @program("TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA") {
     );
 }
 
-mut total_burned: u64;
-mut pda_derived: pubkey;
-
-pub setup() {
-    total_burned = 0;
-}
-
 pub burn_from_pda(
     pda_authority: account @signer,
     token_account: account @mut,
     mint: account @mut,
     token_program: account
-) -> u64 {
+) {
     SPLToken.burn(token_account, mint, pda_authority, 1000);
-    total_burned = total_burned + 1000;
-    return total_burned;
-}
-
-pub get_total_burned() -> u64 {
-    return total_burned;
 }
--- five-templates/cpi-integration-tests/test-spl-token-mint.v
+++ five-templates/cpi-integration-tests/test-spl-token-mint.v
@@ -7,30 +7,11 @@ interface SPLToken @program("TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA") {
     );
 }
 
-mut total_minted: u64;
-mut last_mint_time: u64;
-
-pub setup() {
-    total_minted = 0;
-    last_mint_time = 0;
-}
-
 pub mint_tokens(
     mint: account @mut,
     to: account @mut,
     authority: account @signer,
     token_program: account
-) -> u64 {
+) {
     SPLToken.mint_to(mint, to, authority, 1000);
-    total_minted = total_minted + 1000;
-    return total_minted;
-}
-
-pub mint_variable_amount(
-    mint: account @mut,
-    to: account @mut,
-    authority: account @signer,
-    amount: u64
-) -> u64 {
-    return total_minted;
 }
